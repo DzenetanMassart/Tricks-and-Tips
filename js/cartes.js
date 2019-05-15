7b5f154ce0d22004e6fca88613218e117d755584
DzenetanMassart <dzenetan.massart@gmail.com>
2019-05-15 16:56:22 +0200

    Commencement des fonctionnalités d'affichage en javascript !

---
+++ js/cartes.js
@@ -0,0 +1,18 @@
+let tt = {
+    Canvimation = {
+        badge: "Javascript",
+        img_link: '"http://canvimation.github.io/assets/logo.png"',
+        desc: "Créer un canvas en ligne !",
+        link: '"http://canvimation.github.io/"'
+    }
+
+
+
+
+
+
+
+
+
+
+}
