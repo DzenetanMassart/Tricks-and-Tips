11c8b0a3f2d3f193f35533edcad76958bb08b322
DzenetanMassart <dzenetan.massart@gmail.com>
2019-07-02 15:32:41 +0200

    Ajout du bouton "autre"

--- assets/js/generateur.js
+++ assets/js/generateur.js
@@ -61,6 +61,7 @@ document.getElementById("WordPress").addEventListener("click", wp);
 document.getElementById("JS").addEventListener("click", js);
 document.getElementById("React").addEventListener("click", react);
 document.getElementById("PHP").addEventListener("click", php);
+document.getElementById("Autre").addEventListener("click", autre);
 
 document.getElementById("Gen").addEventListener("click", generators);
 
@@ -429,4 +430,42 @@ function generators() {
 
     }
     document.getElementById("generate").innerHTML = generate;
+}
+
+function autre() {
+    let generate = " ";
+
+    for (let ii = 0; ii < tritip.length; ii++) {
+        let tt = (tritip[ii]);
+
+        if (tt.badge === "") {
+
+            generate += '<a href="' + tt.link + '"><div class="card"> <h4 class = "card-title">' +
+                tt.title +
+                '</h4> <span class = "badge';
+
+            generate += ' ';
+
+            generate += '">' +
+                tt.badge +
+                '</span>';
+            if (tt.generateur === 1) {
+                generate += '<span class = "generateur" > Generateur </span>'
+            }
+            if (tt.img_link != "nope") {
+                generate += '<img class = "card-img-top"src ="' +
+                    tt.img_link +
+                    '" alt ="' +
+                    tt.title +
+                    '" >'
+            } else {}
+
+            generate += '<div class = "card-body"> <p class = "card-text"><span>' +
+                tt.desc +
+                '</span></p></div></div></a>';
+        }
+
+    }
+    document.getElementById("generate").innerHTML = generate;
+
 }
